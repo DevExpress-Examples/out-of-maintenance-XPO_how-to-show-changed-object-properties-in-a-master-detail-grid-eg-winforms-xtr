1258d9a1702ed135970b0663d6f6e0ff4951768d
DevExpressExampleBot <fakeEmail>
2022-10-12 19:21:31 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication25/DomainObject.vb
+++ VB/WindowsFormsApplication25/DomainObject.vb
@@ -1,63 +1,72 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports DevExpress.Xpo
 
 Namespace WindowsFormsApplication25
 
-	Public Class DomainObject
-		Inherits BaseObject
+    Public Class DomainObject
+        Inherits BaseObject
 
-		Public Sub New(ByVal s As Session)
-			MyBase.New(s)
-		End Sub
+        Public Sub New(ByVal s As Session)
+            MyBase.New(s)
+        End Sub
 
-		<Key(True), Persistent("Oid")> _
-		Private _Oid As Guid
-		<PersistentAlias("_Oid")> _
-		Public ReadOnly Property Oid() As Guid
-			Get
-				Return _Oid
-			End Get
-		End Property
+        <Key(True)>
+        <Persistent("Oid")>
+        Private _Oid As Guid
 
-		Private _IntProperty As Integer
-		Public Property IntProperty() As Integer
-			Get
-				Return _IntProperty
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue("IntProperty", _IntProperty, value)
-			End Set
-		End Property
-		Private _DateTimeProperty As DateTime
-		Public Property DateTimeProperty() As DateTime
-			Get
-				Return _DateTimeProperty
-			End Get
-			Set(ByVal value As DateTime)
-				SetPropertyValue("DateTimeProperty", _DateTimeProperty, value)
-			End Set
-		End Property
-		Private _DecimalProperty As Decimal
-		Public Property DecimalProperty() As Decimal
-			Get
-				Return _DecimalProperty
-			End Get
-			Set(ByVal value As Decimal)
-				SetPropertyValue("DecimalProperty", _DecimalProperty, value)
-			End Set
-		End Property
-		Private _StringProperty As String
-		Public Property StringProperty() As String
-			Get
-				Return _StringProperty
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue("StringProperty", _StringProperty, value)
-			End Set
-		End Property
-	End Class
+        <PersistentAlias("_Oid")>
+        Public ReadOnly Property Oid As Guid
+            Get
+                Return _Oid
+            End Get
+        End Property
+
+        Private _IntProperty As Integer
+
+        Public Property IntProperty As Integer
+            Get
+                Return _IntProperty
+            End Get
+
+            Set(ByVal value As Integer)
+                SetPropertyValue("IntProperty", _IntProperty, value)
+            End Set
+        End Property
+
+        Private _DateTimeProperty As Date
+
+        Public Property DateTimeProperty As Date
+            Get
+                Return _DateTimeProperty
+            End Get
+
+            Set(ByVal value As Date)
+                SetPropertyValue("DateTimeProperty", _DateTimeProperty, value)
+            End Set
+        End Property
+
+        Private _DecimalProperty As Decimal
+
+        Public Property DecimalProperty As Decimal
+            Get
+                Return _DecimalProperty
+            End Get
+
+            Set(ByVal value As Decimal)
+                SetPropertyValue("DecimalProperty", _DecimalProperty, value)
+            End Set
+        End Property
+
+        Private _StringProperty As String
+
+        Public Property StringProperty As String
+            Get
+                Return _StringProperty
+            End Get
+
+            Set(ByVal value As String)
+                SetPropertyValue("StringProperty", _StringProperty, value)
+            End Set
+        End Property
+    End Class
 End Namespace
--- VB/WindowsFormsApplication25/Form1.vb
+++ VB/WindowsFormsApplication25/Form1.vb
@@ -1,59 +1,50 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.Xpo
 Imports DevExpress.XtraGrid.Views.Grid
 
 Namespace WindowsFormsApplication25
-	Partial Public Class Form1
-		Inherits Form
 
-		Private uow As UnitOfWork
-		Public Sub New()
-			InitializeComponent()
-			uow = New UnitOfWork()
-			Dim xpc As New XPCollection(Of DomainObject)(uow)
-			gridControl1.DataSource = xpc
+    Public Partial Class Form1
+        Inherits Form
 
-			AddHandler gridView1.RowStyle, AddressOf gridView1_RowStyle
-			AddHandler gridControl1.ViewRegistered, AddressOf gridControl1_ViewRegistered
-		End Sub
+        Private uow As UnitOfWork
 
-		Private Sub gridControl1_ViewRegistered(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.ViewOperationEventArgs)
-			If TypeOf e.View Is GridView Then
-				AddHandler (CType(e.View, GridView)).RowStyle, AddressOf Form1_RowStyle
-			End If
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            uow = New UnitOfWork()
+            Dim xpc As XPCollection(Of DomainObject) = New XPCollection(Of DomainObject)(uow)
+            gridControl1.DataSource = xpc
+            AddHandler gridView1.RowStyle, New RowStyleEventHandler(AddressOf gridView1_RowStyle)
+            AddHandler gridControl1.ViewRegistered, New DevExpress.XtraGrid.ViewOperationEventHandler(AddressOf gridControl1_ViewRegistered)
+        End Sub
 
-		Private Sub Form1_RowStyle(ByVal sender As Object, ByVal e As RowStyleEventArgs)
-			Dim gridView As GridView = CType(sender, GridView)
-			If (Not gridView.IsDataRow(e.RowHandle)) Then
-				Return
-			End If
-			Dim data As PropertyData = TryCast(gridView.GetRow(e.RowHandle), PropertyData)
-			If data IsNot Nothing AndAlso (Not Equals(data.NewValue, data.OldValue)) Then
-				e.Appearance.BackColor = Color.LightSalmon
-			End If
-		End Sub
+        Private Sub gridControl1_ViewRegistered(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.ViewOperationEventArgs)
+            If TypeOf e.View Is GridView Then
+                AddHandler CType(e.View, GridView).RowStyle, New RowStyleEventHandler(AddressOf Form1_RowStyle)
+            End If
+        End Sub
 
-		Private Sub gridView1_RowStyle(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.RowStyleEventArgs)
-			Dim gridView As GridView = CType(sender, GridView)
-			If (Not gridView.IsDataRow(e.RowHandle)) Then
-				Return
-			End If
-			If uow.IsObjectToSave(gridView.GetRow(e.RowHandle)) Then
-				e.Appearance.BackColor = Color.LightSalmon
-			End If
-		End Sub
+        Private Sub Form1_RowStyle(ByVal sender As Object, ByVal e As RowStyleEventArgs)
+            Dim gridView As GridView = CType(sender, GridView)
+            If Not gridView.IsDataRow(e.RowHandle) Then Return
+            Dim data As PropertyData = TryCast(gridView.GetRow(e.RowHandle), PropertyData)
+            If data IsNot Nothing AndAlso Not Equals(data.NewValue, data.OldValue) Then
+                e.Appearance.BackColor = Color.LightSalmon
+            End If
+        End Sub
 
-		Private Sub gridControl1_EmbeddedNavigator_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.NavigatorButtonClickEventArgs) Handles gridControl1.EmbeddedNavigator.ButtonClick
-			uow.CommitChanges()
-		End Sub
-	End Class
+        Private Sub gridView1_RowStyle(ByVal sender As Object, ByVal e As RowStyleEventArgs)
+            Dim gridView As GridView = CType(sender, GridView)
+            If Not gridView.IsDataRow(e.RowHandle) Then Return
+            If uow.IsObjectToSave(gridView.GetRow(e.RowHandle)) Then
+                e.Appearance.BackColor = Color.LightSalmon
+            End If
+        End Sub
+
+        Private Sub gridControl1_EmbeddedNavigator_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.NavigatorButtonClickEventArgs)
+            uow.CommitChanges()
+        End Sub
+    End Class
 End Namespace
